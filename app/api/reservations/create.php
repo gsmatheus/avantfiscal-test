a5b8dd81fd98252b1599330e5ac01fc0133f82b9
Matheus <etec.matheus@gmail.com>
2025-07-11 14:58:48 -0300

    docs: deixando a branch main original novamente

--- app/api/reservations/create.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../../../backend/config/config.php';
-require_once __DIR__ . '/../../../backend/Controllers/ReservationController.php';
-
-$reservationController = new ReservationController();
-$reservationController->create(); 
--- app/api/rooms/create.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../../../backend/config/config.php';
-require_once __DIR__ . '/../../../backend/Controllers/RoomController.php';
-
-$roomController = new RoomController();
-$roomController->create(); 
